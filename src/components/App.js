998d5d9e66fe7031ba0fca5f08e2d6e801204b55
Patrick Gross <pgross41@gmail.com>
2021-01-26 23:39:22 -0600

    Cleanup create-react-app - Fresh slate

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import styles from './App.module.css';
+
+function App() {
+  return (
+    <div className={styles.App}>
+      <header className={styles.header}>
+        <p>
+          Weather Forecast Vis
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
